357115e51988784be7562733c22462fd855fd7dd
George Thayamkery <george.thayamkery@gmail.com>
2020-04-22 16:41:24 -0500

    random db names

--- src/test-database/test-database.module.ts
+++ src/test-database/test-database.module.ts
@@ -10,7 +10,9 @@ import { MongoMemoryServer } from "mongodb-memory-server";
     MongooseModule.forRootAsync({
       useFactory: async () => {
         if (process.env.CI) {
-          return { uri: "mongodb://localhost:27017/db" };
+          // http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
+          const randomDBName = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 20);
+          return { uri: `mongodb://localhost:27017/${randomDBName}` };
         }
         const mongod = new MongoMemoryServer();
         const uri = await mongod.getConnectionString();
