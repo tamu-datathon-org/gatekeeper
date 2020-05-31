81584bd556e691e9def079e2b3c631e3ac90447a
phulsechinmay <phulsechinmay@gmail.com>
2020-05-31 15:35:34 -0500

    Created GalaxyIntegrationGuard for server-side requests between galaxy apps and gatekeeper. * Added ENV variable for galaxy integration secret

---
+++ src/auth/guards/galaxy-integration.guard.ts
@@ -0,0 +1,16 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class GalaxyIntegrationGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    if (request.headers["gatekeeper-integration"] &&
+      request.headers["gatekeeper-integration"] === process.env.GATEKEEPER_INTEGRATION_SECRET) {
+      return true;
+    }
+    return false;
+  }
+}
