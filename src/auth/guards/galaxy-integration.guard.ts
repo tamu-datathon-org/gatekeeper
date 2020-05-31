476a19d3e558c6fa4f378b7520b41576b34cb628
phulsechinmay <phulsechinmay@gmail.com>
2020-05-31 15:37:39 -0500

    Linting fixes

--- src/auth/guards/galaxy-integration.guard.ts
+++ src/auth/guards/galaxy-integration.guard.ts
@@ -1,14 +1,17 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class GalaxyIntegrationGuard implements CanActivate {
   canActivate(
-    context: ExecutionContext,
+    context: ExecutionContext
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-    if (request.headers["gatekeeper-integration"] &&
-      request.headers["gatekeeper-integration"] === process.env.GATEKEEPER_INTEGRATION_SECRET) {
+    if (
+      request.headers["gatekeeper-integration"] &&
+      request.headers["gatekeeper-integration"] ===
+        process.env.GATEKEEPER_INTEGRATION_SECRET
+    ) {
       return true;
     }
     return false;
