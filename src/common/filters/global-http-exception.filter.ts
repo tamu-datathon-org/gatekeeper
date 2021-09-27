1e8f0864d4e592cf43e19f42fc77c89465511931
George Thayamkery <george.thayamkery@gmail.com>
2021-09-27 17:20:53 -0500

    sigh

--- src/common/filters/global-http-exception.filter.ts
+++ src/common/filters/global-http-exception.filter.ts
@@ -12,59 +12,69 @@ import fetch from "node-fetch";
 @Catch()
 export class GlobalHttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
-    // Send server exceptions to r2d2 slackbot, if in production mode.
-    if (process.env.NODE_ENV === "prod" && exception.getStatus() >= 500) {
-      const payload = {
-        source: "GATEKEEPER",
-        error: `${exception.stack}\n${JSON.stringify(exception, undefined, 2)}`,
-      };
-      fetch(`${process.env.SLACKBOT_BASE_URL}slack/log-error`, {
-        method: "post",
-        body: JSON.stringify(payload, undefined, 2),
-        headers: {
-          "Content-Type": "application/json",
-          "Gatekeeper-Integration": process.env
-            .GATEKEEPER_INTEGRATION_SECRET as string,
-        },
-      });
-    }
+    console.error(exception);
+    try {
 
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest();
+      // Send server exceptions to r2d2 slackbot, if in production mode.
+      if (process.env.NODE_ENV === "prod" && exception.getStatus && exception.getStatus() >= 500) {
+        const payload = {
+          source: "GATEKEEPER",
+          error: `${exception.stack}\n${JSON.stringify(exception, undefined, 2)}`,
+        };
+        fetch(`${process.env.SLACKBOT_BASE_URL}slack/log-error`, {
+          method: "post",
+          body: JSON.stringify(payload, undefined, 2),
+          headers: {
+            "Content-Type": "application/json",
+            "Gatekeeper-Integration": process.env
+              .GATEKEEPER_INTEGRATION_SECRET as string,
+          },
+        });
+      }
 
-    // don't render views or do redirects if its an api request
-    const isAPIRequest = request.headers.accept === "application/json";
+      const ctx = host.switchToHttp();
+      const response = ctx.getResponse<Response>();
+      const request = ctx.getRequest();
 
-    if (isAPIRequest && exception instanceof HttpException) {
-      return response
-        .status(exception.getStatus())
-        .json(exception.getResponse());
-    } else if (isAPIRequest && !(exception instanceof HttpException)) {
-      console.error(exception);
+      // don't render views or do redirects if its an api request
+      const isAPIRequest = request.headers.accept === "application/json";
+
+      if (isAPIRequest && exception instanceof HttpException) {
+        return response
+          .status(exception.getStatus())
+          .json(exception.getResponse());
+      } else if (isAPIRequest && !(exception instanceof HttpException)) {
+        console.error(exception);
+        return response
+          .status(500)
+          .json(new HttpException("Internal Server Error", 500));
+      }
+
+      // render views if it is not an API request
+      if (exception instanceof UserNotVerifiedException) {
+        return response.render("signup/resend-verification-email.ejs", {
+          redirectLink: `/auth${request.path}`,
+        });
+      } else if (exception instanceof UnauthorizedException) {
+        return response.redirect(`/auth/login?r=/auth${request.path}`);
+      }
+
+      // Default Exception Case: Just return the error
+      if (exception instanceof HttpException)
+        // turns out normal Errors can come through here
+        return response.status(exception.getStatus()).json(exception);
+      else {
+        console.error(exception);
+        return response
+          .status(500)
+          .json(new HttpException("Internal Server Error", 500));
+      }
+    } catch(e) {
+      console.error("GlobalExceptionHandlerError, oh the irony", e);
       return response
         .status(500)
         .json(new HttpException("Internal Server Error", 500));
     }
 
-    // render views if it is not an API request
-    if (exception instanceof UserNotVerifiedException) {
-      return response.render("signup/resend-verification-email.ejs", {
-        redirectLink: `/auth${request.path}`,
-      });
-    } else if (exception instanceof UnauthorizedException) {
-      return response.redirect(`/auth/login?r=/auth${request.path}`);
-    }
-
-    // Default Exception Case: Just return the error
-    if (exception instanceof HttpException)
-      // turns out normal Errors can come through here
-      return response.status(exception.getStatus()).json(exception);
-    else {
-      console.error(exception);
-      return response
-        .status(500)
-        .json(new HttpException("Internal Server Error", 500));
-    }
   }
 }
