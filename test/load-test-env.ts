6a90e2dd603caaaf351b4eb01d85575a8759363e
Christopher Vu <65040991+S1nthesis@users.noreply.github.com>
2023-01-13 14:24:56 -0600

    resolving lint error hopefully

--- test/load-test-env.ts
+++ test/load-test-env.ts
@@ -1,4 +1,5 @@
-require("dotenv").config({ path: ".env.test" });
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.test" });
 
 if (process.env.NODE_ENV !== "test") {
   throw Error("Test environment mismatch!");
